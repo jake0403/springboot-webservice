3685793bf4342c4595d26f3e3e69b6be8614a92f
JAESANG LEE <jake0430@naver.com>
2020-05-12 09:17:12 +0900

    dd

--- scripts/start.sh
+++ scripts/start.sh
@@ -20,3 +20,5 @@ echo "> 새 애플리케이션($JAR_NAME) 배포"
 nohup java -jar \
 	-Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profiles.active=$IDLE_PROFILE \
 	$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+	
+	
